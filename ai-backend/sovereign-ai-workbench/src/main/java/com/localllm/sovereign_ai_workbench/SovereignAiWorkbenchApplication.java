03d6eb9e62233f3b950a25fbf8de2f77db0f01bf
NavtejSutar <navtejsutar@gmail.com>
2026-08-29 20:47:45 +0530

    spring initailizer

---
+++ ai-backend/sovereign-ai-workbench/src/main/java/com/localllm/sovereign_ai_workbench/SovereignAiWorkbenchApplication.java
@@ -0,0 +1,13 @@
+package com.localllm.sovereign_ai_workbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovereignAiWorkbenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SovereignAiWorkbenchApplication.class, args);
+	}
+
+}
---
+++ ai-backend/sovereign-ai-workbench/src/test/java/com/localllm/sovereign_ai_workbench/SovereignAiWorkbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localllm.sovereign_ai_workbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SovereignAiWorkbenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
